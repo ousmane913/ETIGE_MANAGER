ff49b4ba36d2e8de183fe52784c5ed52dae389d0
ousmane913 <odiarrassouba5507@gmail.com>
2026-08-24 02:34:24 +0000

    Fix build.sh to compile npm assets inside frontend directory

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@ set -o errexit
 
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+cd frontend
 npm ci
 npm run build
+cd ..
 python manage.py collectstatic --noinput
 python manage.py check --deploy
 python manage.py migrate
