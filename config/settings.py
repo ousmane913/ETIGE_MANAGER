c191c4d3187eedbf19ff5361d1af654e2c1b0f8b
ousmane913 <odiarrassouba5507@gmail.com>
2026-09-08 13:15:17 +0000

    feat: support EMAIL_USE_SSL pour port 465

--- config/settings.py
+++ config/settings.py
@@ -91,6 +91,7 @@ EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False').lower() == 'true'
 EMAIL_TIMEOUT = 15
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'ETIGE <contact@etige.ci>')
 
